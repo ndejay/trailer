ebd8d94e3a9e92f04de88ad43377dd8481b4cd66
Nicolas De Jay <ndj@pinkfilter.org>
2015-05-02 22:51:07 -0400

    Implement Message#create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,14 @@ class MessagesController < ApplicationController
   end
 
   def create
-    render plain: params[:message].inspect
+    @message = Message.new(message_params)
+
+    @message.save
+    redirect_to @message
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:text)
   end
 end
