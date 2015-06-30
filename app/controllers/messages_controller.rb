4a0e5d33dfd71b8b64af1def32c08c7ae6f41d13
Nicolas De Jay <ndj@pinkfilter.org>
2015-06-29 22:57:38 -0400

    Implement Message#{show,index}

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,12 @@
 class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+
+  def show
+    @message = Message.find(params[:id])
+  end
+
   def new
   end
 
