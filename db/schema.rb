fe4402518ca11f20460b13420c813cfcb1489226
Steven Hebert <steven.hebert@umontreal.ca>
2015-12-02 21:19:18 -0500

    Add users model and migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630024435) do
+ActiveRecord::Schema.define(version: 20151203021422) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
