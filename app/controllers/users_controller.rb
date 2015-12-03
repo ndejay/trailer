c015f4f174ea2150dcba11d29254eacff91df8db
Steven Hebert <steven.hebert@umontreal.ca>
2015-12-02 21:38:19 -0500

    Implement User#show and User#index.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:user].inspect
+    @user = User.new(user_params)
+
+    @user.save
+    redirect_to @user
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :password)
   end
 end
