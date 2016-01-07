8e3d78144dd5d6e8275d3066c9fd9c16ace5c66a
Nicolas De Jay <ndj@pinkfilter.org>
2016-01-06 20:39:54 -0500

    Test messages controller for index and create.

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class MessagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    # @request.headers["Accept"] = "text/plain, text/html"
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:messages)
+  end
+
+  test "should create message" do
+    assert_difference('Message.count') do
+      # @request.headers["HTTP_REFERER"] = "http://example.com/home"
+      post :create, message: {text: 'Some content'}
+    end
+
+    assert_redirected_to message_path(assigns(:message))
+  end
 end
